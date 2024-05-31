2a4f293cec13a2658326aaba0c4ae3d8fdd73c62
ngelkyutie <ngelsarandin@gmail.com>
2024-05-31 12:58:44 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,188 @@
+let closer = document.querySelector('#closer');
+
+closer.onclick = () => {
+    closer.style.display = 'none';
+    navbar.classList.remove('active');
+    cart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    closer.style.display = "block";
+    navbar.classList.toggle('active');
+}
+
+let cart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () => {
+    closer.style.display = "block";
+    cart.classList.toggle('active');
+}
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () => {
+    closer.style.display = "block";
+    loginForm.classList.toggle('active');
+}
+
+let searchForm = document.querySelector('.header .search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    searchForm.classList.remove('active');
+}
+
+let slides = document.querySelectorAll('.home .slides-container .slide');
+let index = 0;
+
+function next() {
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev() {
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+
+
+  let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'Shun 2 Seater Sofa',
+        image: 'image/2-Seater-Sofa.png',
+        price: 19950
+    },
+    {
+        id: 2,
+        name: 'Shun 3 Seater Sofa',
+        image: 'image/3-Seater-Sofa.png',
+        price: 19950
+    },
+    {
+        id: 3,
+        name: 'Chriselli Sectional Sofa',
+        image: 'image/CHRISELLI_1000x.png',
+        price: 39950
+    },
+    {
+        id: 4,
+        name: 'Karmi Sectinal Sofa',
+        image: 'image/SURREY-_3_abce3bca-213b-489c-91f0-cbcaaf43ce81_1000x.png',
+        price: 79960
+    },
+    {
+        id: 5,
+        name: 'Gracen 3 Seater Sofa',
+        image: 'image/Gracen-3S-2-Corner-1-Ottoman-1_10424620-_7_60b39fb6-a3b0-4ea4-85da-53b1f4dfd3dd_1000x.png',
+        price: 44950
+    },
+    {
+        id: 6,
+        name: 'Cleburne 3 Seater Sofa',
+        image: 'image/10415901_Cleburne3SeaterSofa_front_43cb9c02-80d8-458b-94d2-e8ae72d54263_1000x.png',
+        price: 27950
+    }
+];
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Add To Card</button>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
+function addToCard(key){
+    if(listCards[key] == null){
+        // copy product form list to list card
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+    }
+    reloadCard();
+}
+function reloadCard(){
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="image/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCard();
+}
+
+
+
+
+
+
+
